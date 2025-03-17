3f5b3db15a4b33cbb122631e18d8d54c48d07e19
shiptonbijal <shiptonbijal@gmail.com>
2025-03-17 09:32:01 +0000

    Add 12.rs

---
+++ src/12.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x == 10 {
+        println!("Ten!");
+    } else if x < 5 {
+        println!("Less than five!");
+    } else {
+        println!("Greater than five!");
+    }
+}
