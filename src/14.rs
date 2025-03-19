c1d8087c9440ea0902ed2329ebbe12ade8e70ed4
shiptonbijal <shiptonbijal@gmail.com>
2025-03-19 15:16:55 +0000

    Add 14.rs

---
+++ src/14.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let x = 5;
+    let y = 6;
+    if x + y > 10 {
+        println!("The sum is greater than 10");
+    } else {
+        println!("The sum is not greater than 10");
+    }
+}
